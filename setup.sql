c7952e68609a85f94b7147ca7e5bc362d3b51cb0
Andrew Henry <ajh145@pitt.edu>
2018-12-27 14:08:49 -0500

    Updated setup with permission setup

--- setup.sql
+++ setup.sql
@@ -7,13 +7,22 @@ Yam Database
 -- First need to delete everything
 DROP SCHEMA yamschema CASCADE;
 CREATE SCHEMA yamschema;
-ALTER DATABASE yamdb SET search_path TO yamschema;
 
 SET SCHEMA 'yamschema';
+
+-- Extensions go here
+CREATE EXTENSION postgis;
+CREATE EXTENSION citext;
+
 BEGIN;
 \i setup-files/tables.sql
 \i setup-files/functions.sql
 \i setup-files/updates.sql
 \i setup-files/datainsert.sql
 COMMIT;
+
+-- permissions have be last, as there is nothing created before
+GRANT USAGE ON SCHEMA yamschema TO yamuser;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA yamschema TO yamuser;
+ALTER DATABASE yamdb SET search_path TO yamschema;
 
